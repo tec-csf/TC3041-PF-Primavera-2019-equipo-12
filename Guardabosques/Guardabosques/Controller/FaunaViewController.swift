1b2dd9f7186a495e4f061f6723ab185cb9490564
JJ Santos <jj@HAL-9031.local>
2019-04-23 20:16:50 -0500

    Menu Completed. Working on tableviews

---
+++ Guardabosques/Guardabosques/Controller/FaunaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FaunaViewController.swift
+//  Guardabosques
+//
+//  Created by JJ Santos on 4/23/19.
+//  Copyright © 2019 JJ Santos. All rights reserved.
+//
+
+import UIKit
+
+class FaunaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
