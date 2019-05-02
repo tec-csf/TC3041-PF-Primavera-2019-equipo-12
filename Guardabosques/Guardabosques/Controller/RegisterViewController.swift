865bd27942ffd315a56255ae9bc10c1481abdc23
JJ Santos <jj@HAL-9031.local>
2019-05-01 21:45:16 -0500

    SQLITE CREATE AND INSERT

--- Guardabosques/Guardabosques/Controller/RegisterViewController.swift
+++ Guardabosques/Guardabosques/Controller/RegisterViewController.swift
@@ -9,29 +9,87 @@
 import UIKit
 import Firebase
 import SVProgressHUD
+import SQLite3
 
-class RegisterViewController: UIViewController {
+class RegisterViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
-    
+    var db: OpaquePointer?
+    var location: String!
+    @IBOutlet var jobTextField: UITextField!
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var emailTextfield: UITextField!
     @IBOutlet var passwordTextfield: UITextField!
     
+    @IBOutlet weak var picker: UIPickerView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        
+        //the SQLITE database file
+        let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            .appendingPathComponent("Guardabosques.sqlite")
+        
+        //opening the database
+        if sqlite3_open(fileURL.path, &db) != SQLITE_OK {
+            print("error opening database")
+        }
+        
+        //creating table
+        if sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, job TEXT, location TEXT)", nil, nil, nil) != SQLITE_OK {
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("error creating table: \(errmsg)")
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
         emailTextfield.text = ""
         passwordTextfield.text = ""
         nameTextField.text = ""
+        jobTextField.text = ""
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return locations.count
+    }
+    
+
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        
+        let label: UILabel
+        
+        if let view = view {
+            label = view as! UILabel
+        }
+        else {
+            label = UILabel(frame: CGRect(x: 0, y: 0, width: pickerView.frame.width, height: 400))
+        }
+        
+        let title = locations[row]
+        let attributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        label.attributedText = NSAttributedString(string: title, attributes: attributes)
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.sizeToFit()
+        
+        return label
+    }
+    
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        location = locations[row]
+    }
     
     
     @IBAction func registerPressed(_ sender: AnyObject) {
@@ -51,9 +109,68 @@ class RegisterViewController: UIViewController {
             } else {
                 print("Registration Successful!")
                 
-                SVProgressHUD.dismiss()
+            
+                // Only if Firebase register was succesful, then update local SQLite Database
+                let name = self.nameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+                let job = self.jobTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
                 
-                self.performSegue(withIdentifier: "goToLocations", sender: self)
+                
+                
+                
+                print("name:",name)
+                print("job",job)
+                print("location",self.location)
+                
+                    //.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+              
+                //creating a statement
+                var stmt: OpaquePointer?
+                
+                //the insert query
+                let queryString = "INSERT INTO Users (name, job, location) VALUES (?,?,?)"
+                
+                //preparing the query
+                if sqlite3_prepare(self.db, queryString, -1, &stmt, nil) != SQLITE_OK{
+                    let errmsg = String(cString: sqlite3_errmsg(self.db)!)
+                    print("error preparing insert: \(errmsg)")
+                    return
+                }
+                
+                //binding the parameters
+                if sqlite3_bind_text(stmt, 1, name, -1, nil) != SQLITE_OK{
+                    let errmsg = String(cString: sqlite3_errmsg(self.db)!)
+                    print("failure binding name: \(errmsg)")
+                    return
+                }
+                if sqlite3_bind_text(stmt, 2, job, -1, nil) != SQLITE_OK{
+                    let errmsg = String(cString: sqlite3_errmsg(self.db)!)
+                    print("failure binding name: \(errmsg)")
+                    return
+                }
+                if sqlite3_bind_text(stmt, 3, self.location, -1, nil) != SQLITE_OK{
+                    let errmsg = String(cString: sqlite3_errmsg(self.db)!)
+                    print("failure binding name: \(errmsg)")
+                    return
+                }
+                
+                
+                
+                //executing the query to insert values
+                if sqlite3_step(stmt) != SQLITE_DONE {
+                    let errmsg = String(cString: sqlite3_errmsg(self.db)!)
+                    print("failure inserting User: \(errmsg)")
+                    return
+                }
+                
+                
+                //displaying a success message
+                print("User saved successfully")
+                
+                SVProgressHUD.dismiss()
+                self.performSegue(withIdentifier: "goToMenu", sender: self)
+                
+                
+                
             }
         }
         
