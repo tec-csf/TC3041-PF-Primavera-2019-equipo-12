a76e5717a6369e67375637de3b7d8278e0498b26
JJ Santos <jj@HAL-9031.local>
2019-05-01 19:41:11 -0500

    Profile and Logout

--- Guardabosques/Guardabosques/Controller/ProfileViewController.swift
+++ Guardabosques/Guardabosques/Controller/ProfileViewController.swift
@@ -7,8 +7,15 @@
 //
 
 import UIKit
+import Firebase
 
 class ProfileViewController: UIViewController {
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,14 +24,22 @@ class ProfileViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   
+    @IBAction func logout(_ sender: Any) {
+        
+        do {
+            try Auth.auth().signOut()
+            
+            navigationController?.popToRootViewController(animated: true)
+            
+        }
+        catch {
+            print("error: there was a problem logging out")
+        }
+        
     }
-    */
+    
+    
+    
 
 }
